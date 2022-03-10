17e0a982a92073070c581603989a0740d167f56e
Swise99 <samuel@inprof.com>
2022-03-09 19:16:07 -0500

    Fixed error bug

--- A1.js
+++ A1.js
@@ -7,9 +7,9 @@ function addnames(){
     var player1 = new playerinfo(1,document.getElementById("P1name").value);
     var player2 = new playerinfo(2,document.getElementById("P2name").value);
     if (player1.name==''){
-        document.getElementById("errorspace").innerHTML="Please enter Player 1 name"
+        document.getElementById("error-space").innerHTML="Please enter Player 1 name"
     } else if (player2.name==''){
-        document.getElementById("error space").innerHTML="Please enter Player 2 name"
+        document.getElementById("error-space").innerHTML="Please enter Player 2 name"
     } else {
         player1.ready=true
         player2.ready=true
