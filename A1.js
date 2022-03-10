62a3b9617f722fb1c829bd7859f0947a3ea4a6ac
Swise99 <samuel@inprof.com>
2022-03-09 19:10:18 -0500

    player addition completed

--- A1.js
+++ A1.js
@@ -6,5 +6,15 @@ document.getElementById("nameinput").onclick = function(){addnames()};
 function addnames(){
     var player1 = new playerinfo(1,document.getElementById("P1name").value);
     var player2 = new playerinfo(2,document.getElementById("P2name").value);
-    console.log(player1.name);
+    if (player1.name==''){
+        document.getElementById("errorspace").innerHTML="Please enter Player 1 name"
+    } else if (player2.name==''){
+        document.getElementById("error space").innerHTML="Please enter Player 2 name"
+    } else {
+        player1.ready=true
+        player2.ready=true
+    }
+    console.log("Hello"+player1.name+player1.ready);
+    console.log("Hello"+player2.name+player2.ready);
 }
+
