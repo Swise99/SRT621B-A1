08c856000596820ee11195b5daef3b337bb10f22
Samuel Wise <samuel@inprof.com>
2022-03-16 11:58:22 -0400

    Game grid added

--- A1.js
+++ A1.js
@@ -2,9 +2,14 @@ function playerinfo(number, name) {
     this.name=name;
     this.number=number;
 }
-var currentPlayer;
+const statusDisplay = document.getElementById('turn-counter');
+var currentPlayer='';
 const player1 = new playerinfo;
 const player2 = new playerinfo;
+let cellValues = ["", "", "", "", "", "", "", "", ""];
+const winText = () => `Player ${currentPlayer} has won!`;
+const drawText = () => `Game ended in a draw!`;
+const currentPlayerText = () => `It's ${currentPlayer}'s turn`;
 document.getElementById("nameinput").onclick = function addnames(){
     player1.name =document.getElementById("P1name").value;
     player2.name =document.getElementById("P2name").value;
@@ -16,9 +21,14 @@ document.getElementById("nameinput").onclick = function addnames(){
         player1.ready=true;
         player2.ready=true;
         currentPlayer=player1.name;
-        document.getElementById("turn-counter").innerHTML= "Current turn: "+currentPlayer;
+        var x = document.getElementById("container");
+        x.style.display = "grid";
+        statusDisplay.innerHTML = currentPlayerText();
     }
     console.log("Hello"+player1.name+player1.ready);
     console.log("Hello"+player2.name+player2.ready);
 };
+// function performCellClick() {
 
+// }
+// document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick))
